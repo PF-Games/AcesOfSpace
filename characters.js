bd7c1342f8ce2b8cac748f9d351fcc50b2221636
PF-Gaming <pablodanielfacelli@gmail.com>
2025-10-08 11:00:54 -0300

    create class for main ships
    We will need these classes to create the main ships of the board

---
+++ characters.js
@@ -0,0 +1,20 @@
+class Character extends Entity {
+  constructor(texture, x, y, game) {
+    super(texture, x, y, game);
+ }
+}
+
+
+
+class Enemy extends Character{
+  constructor(texture, x, y, game) {
+    super(texture, x, y, game);
+ }
+}
+
+
+class MainChar extends Character{
+  constructor(texture, x, y, game) {
+    super(texture, x, y, game);
+ }
+}
